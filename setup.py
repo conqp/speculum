cb76b59b3374914aeefc015efaac8178c5343da1
Richard Neumann <r.neumann@homeinfo.de>
2019-03-05 10:19:57 +0100

    Updated version.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from distutils.core import setup
 
 setup(
     name='speculum',
-    version='1.4.2',
+    version='1.4.3',
     author='Richard Neumann',
     author_email='<mail at richard dash neumann period de>',
     maintainer='Richard Neumann',
