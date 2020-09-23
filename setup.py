7872a122d3e749aef5a054e319ba27fb9fcd4e69
Richard Neumann <mail@richard-neumann.de>
2020-09-24 00:58:26 +0200

    Bumped version.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from distutils.core import setup
 
 setup(
     name='speculum',
-    version='1.4.9',
+    version='1.5.0',
     author='Richard Neumann',
     author_email='<mail at richard dash neumann period de>',
     maintainer='Richard Neumann',
