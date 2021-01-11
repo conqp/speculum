82398727070f55fd27be68cfd886588fa047b523
Richard Neumann <mail@richard-neumann.de>
2021-01-11 10:23:38 +0100

    Refactored tests.

--- tests/test_limiting.py
+++ tests/test_limiting.py
@@ -1,5 +1,7 @@
 """Tests the limiting module."""
 
+from json import load
+from pathlib import Path
 from unittest import TestCase
 
 from speculum.limiting import limit
@@ -10,17 +12,20 @@ class TestLimit(TestCase):
 
     def setUp(self):
         """Sets example iterables."""
-        self.iterable = [1, 2.0, 3, 'foo', 'bar', {'spamm': 'eggs'}]
+        with Path(__file__).parent.joinpath('mirrors.json').open() as file:
+            json = load(file)
+
+        self.mirrors = json['urls']
 
     def test_limit(self):
         """Tests the limit function."""
-        for maxlen in range(len(self.iterable)):
+        for maxlen in range(len(self.mirrors)):
             self.assertSequenceEqual(
-                list(limit(self.iterable, maxlen)),
-                self.iterable[:maxlen]
+                list(limit(self.mirrors, maxlen)),
+                self.mirrors[:maxlen]
             )
 
         self.assertSequenceEqual(
-            list(limit(self.iterable, None)),
-            self.iterable
+            list(limit(self.mirrors, None)),
+            self.mirrors
         )
