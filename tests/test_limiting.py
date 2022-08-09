b0cf12bcdbc22cc74e1078219733eb9bda8c5d9b
Richard Neumann <mail@richard-neumann.de>
2022-08-10 01:25:30 +0200

    Refactor tests

--- tests/test_limiting.py
+++ tests/test_limiting.py
@@ -7,22 +7,25 @@ from unittest import TestCase
 from speculum.limiting import limit
 
 
+MIRRORS_FILE = Path(__file__).parent / 'mirrors.json'
+
+
 class TestLimit(TestCase):
     """Tests the limit() function."""
 
     def setUp(self):
         """Sets example iterables."""
-        with Path(__file__).parent.joinpath('mirrors.json').open() as file:
+        with MIRRORS_FILE.open() as file:
             json = load(file)
 
         self.mirrors = json['urls']
 
     def test_limit(self):
         """Tests the limit function."""
-        for maxlen in range(len(self.mirrors)):
+        for max_len in range(len(self.mirrors)):
             self.assertSequenceEqual(
-                list(limit(self.mirrors, maxlen)),
-                self.mirrors[:maxlen]
+                list(limit(self.mirrors, max_len)),
+                self.mirrors[:max_len]
             )
 
         self.assertSequenceEqual(
