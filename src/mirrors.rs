24b4119866b8df9ebba248c5525f6294c2056c03
Richard Neumann <mail@richard-neumann.de>
2026-01-11 10:04:21 +0100

    Implement getters.

--- src/mirrors.rs
+++ src/mirrors.rs
@@ -18,12 +18,48 @@ pub struct Mirrors {
 }
 
 impl Mirrors {
+    /// Return the cutoff value.
+    #[must_use]
+    pub const fn cutoff(&self) -> u64 {
+        self.cutoff
+    }
+
+    /// Return the timestamp of the last check.
+    #[must_use]
+    pub const fn last_check(&self) -> DateTime<FixedOffset> {
+        self.last_check
+    }
+
+    /// Return the amount of checks.
+    #[must_use]
+    pub const fn num_checks(&self) -> usize {
+        self.num_checks
+    }
+
+    /// Return the check frequency.
+    #[must_use]
+    pub const fn check_frequency(&self) -> u64 {
+        self.check_frequency
+    }
+
+    /// Return the mirror list.
+    #[must_use]
+    pub fn urls(&self) -> &[Mirror] {
+        &self.urls
+    }
+
+    /// Return the API version.
+    #[must_use]
+    pub const fn version(&self) -> u8 {
+        self.version
+    }
+
     /// Get the mirrors from the given URL.
     ///
     /// # Errors
     ///
     /// Returns an [`reqwest::Error`] if the request could not be executed successfully.
-    pub async fn get<T>(url: T) -> reqwest::Result<Mirrors>
+    pub async fn get<T>(url: T) -> reqwest::Result<Self>
     where
         T: IntoUrl,
     {
