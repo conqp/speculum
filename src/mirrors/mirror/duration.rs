d139092aca90c99324290ada8a9acb2abdf5e2ce
Richard Neumann <mail@richard-neumann.de>
2026-01-11 10:06:47 +0100

    Implement getters.

--- src/mirrors/mirror/duration.rs
+++ src/mirrors/mirror/duration.rs
@@ -1,10 +1,24 @@
 use serde::Deserialize;
 
 /// Duration information.
-#[derive(Clone, Debug, PartialEq, Deserialize)]
+#[derive(Clone, Copy, Debug, PartialEq, Deserialize)]
 pub struct Duration {
     #[serde(rename = "duration_avg")]
     average: f64,
     #[serde(rename = "duration_stddev")]
     standard_deviation: f64,
+}
+
+impl Duration {
+    /// Return the average duration.
+    #[must_use]
+    pub const fn average(self) -> f64 {
+        self.average
+    }
+
+    /// Return the standard deviation of the duration.
+    #[must_use]
+    pub const fn standard_deviation(self) -> f64 {
+        self.standard_deviation
+    }
 }
