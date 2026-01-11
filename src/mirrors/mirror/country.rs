26ad487da51bca8f80fbdab69cebdbc602098965
Richard Neumann <mail@richard-neumann.de>
2026-01-11 10:35:19 +0100

    Manually implement `PartialEq` for `Country`.

--- src/mirrors/mirror/country.rs
+++ src/mirrors/mirror/country.rs
@@ -1,15 +1,28 @@
+use std::ops::Deref;
+
 use serde::Deserialize;
 
 /// Country information.
-#[derive(Clone, Debug, Eq, PartialEq, Hash, Deserialize)]
-pub struct Country {
+#[derive(Clone, Debug, Hash, Deserialize)]
+pub struct Country<T> {
     #[serde(rename = "country")]
-    name: String,
+    name: T,
     #[serde(rename = "country_code")]
-    code: String,
+    code: T,
 }
 
-impl Country {
+impl Country<&'static str> {}
+
+impl<T> Country<T>
+where
+    T: Deref<Target = str>,
+{
+    /// Create a new `Country`.
+    #[must_use]
+    pub const fn new(name: T, code: T) -> Self {
+        Self { name, code }
+    }
+
     /// Return the country's name.
     #[must_use]
     pub fn name(&self) -> &str {
@@ -20,5 +33,15 @@ impl Country {
     #[must_use]
     pub fn code(&self) -> &str {
         &self.code
+    }
+}
+
+impl<T, U> PartialEq<Country<U>> for Country<T>
+where
+    T: Deref<Target = str>,
+    U: Deref<Target = str>,
+{
+    fn eq(&self, other: &Country<U>) -> bool {
+        (self.code() == other.code()) || (self.name() == other.name())
     }
 }
